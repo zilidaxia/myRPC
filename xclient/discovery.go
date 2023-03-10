3a8342d0291f273740a55fb29e98ea285d3298f0
myname <2214736768@qq.com>
2023-03-10 14:40:21 +0800

    基础服务发现模块

---
+++ xclient/discovery.go
@@ -0,0 +1,89 @@
+package xclient
+
+//服务发现模块
+import (
+	"errors"
+	"math"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+type SelectMode int
+
+// 简单实现Random和RoundRobin（ 依次调度不同的服务器，每次调度执行 i = (i + 1) mode n。）两种负载均衡策略
+const (
+	RandomSelect     SelectMode = iota // select randomly
+	RoundRobinSelect                   // select using Robbin algorithm
+)
+
+type Discovery interface {
+	Refresh() error // refresh from remote registry
+	Update(servers []string) error
+	Get(mode SelectMode) (string, error)
+	GetAll() ([]string, error)
+}
+
+// MultiServersDiscovery 先实现一个不需要注册中心，服务列表由手工维护的服务发现的结构体
+// user provides the server addresses explicitly instead
+type MultiServersDiscovery struct {
+	r       *rand.Rand   // generate random number
+	mu      sync.RWMutex // protect following
+	servers []string
+	index   int // 记录轮询算法已经轮询到的位置，避免每次从0开始，初始化时随机设定一个值
+}
+
+// NewMultiServerDiscovery creates a MultiServersDiscovery instance
+func NewMultiServerDiscovery(servers []string) *MultiServersDiscovery {
+	d := &MultiServersDiscovery{
+		servers: servers,
+		r:       rand.New(rand.NewSource(time.Now().UnixNano())),
+	}
+	d.index = d.r.Intn(math.MaxInt32 - 1)
+	return d
+}
+
+var _ Discovery = (*MultiServersDiscovery)(nil)
+
+// Refresh 从注册中心更新服务列表
+func (d *MultiServersDiscovery) Refresh() error {
+	return nil
+}
+
+// Update 手动更新服务列表
+func (d *MultiServersDiscovery) Update(servers []string) error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	d.servers = servers
+	return nil
+}
+
+// Get 根据负载均衡策略选择一个服务实例
+func (d *MultiServersDiscovery) Get(mode SelectMode) (string, error) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	n := len(d.servers)
+	if n == 0 {
+		return "", errors.New("rpc discovery: no available servers")
+	}
+	switch mode {
+	case RandomSelect:
+		return d.servers[d.r.Intn(n)], nil
+	case RoundRobinSelect:
+		s := d.servers[d.index%n] // servers could be updated, so mode n to ensure safety
+		d.index = (d.index + 1) % n
+		return s, nil
+	default:
+		return "", errors.New("rpc discovery: not supported select mode")
+	}
+}
+
+// GetAll 返回所有的服务实例
+func (d *MultiServersDiscovery) GetAll() ([]string, error) {
+	d.mu.RLock()
+	defer d.mu.RUnlock()
+	// return a copy of d.servers
+	servers := make([]string, len(d.servers), len(d.servers))
+	copy(servers, d.servers)
+	return servers, nil
+}
